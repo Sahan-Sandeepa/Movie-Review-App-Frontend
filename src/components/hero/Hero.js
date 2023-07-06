ca2587aee576246dfb1ec6f001e05ceb0f3062b4
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-07-06 22:19:40 +0530

    made video trailer component

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -41,13 +41,13 @@ const Hero = ({movies}) => {
                                                 <h4>{movie.title}</h4>
                                             </div>
                                             <div className="movie-buttons-container">
-                                                {/* <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}> */}
+                                                <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
                                                     <div className="play-button-icon-container">
                                                         <FontAwesomeIcon className="play-button-icon"
                                                             icon={faCirclePlay}
                                                         />
                                                     </div>
-                                                {/* </Link> */}
+                                                </Link>
 
                                                 <div className="movie-review-button-container">
                                                     <Button variant="info" onClick={() => reviews(movie.imdbId)} >Reviews</Button>

85b0d6c401851c0bada1e449617c5b9d7f56095e
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-07-13 18:40:47 +0530

    took review data from mongo

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../hero/Hero'
 
 const Home = ({movies}) => {
-  console.log(movies)
+  // console.log(movies)
   return (
     <Hero movies={movies}/>
   )

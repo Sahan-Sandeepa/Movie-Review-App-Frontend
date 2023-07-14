80c98c39bf6a7484b795b501bef90e014de8ce21
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-07-14 22:10:16 +0530

    implemented edit and delete feature

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,72 +1,82 @@
 import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Form, FormGroup, FormLabel, FormControl } from "react-bootstrap";
 import ReviewForm from "../reviewForm/ReviewForm";
 import React from 'react';
-
 const Reviews = ({ getMovieData, movie, reviews = [], setReviews }) => {
-    const [review, setReview] = useState([]);
     const [getreview, setGetReview] = useState([]);
-    const revText = useRef();
+    // const revText = useRef();
+    const [editingReview, setEditingReview] = useState(null); // Track the review being edited
     let params = useParams();
     const movieId = params.movieId;
+    const [rev, setRev] = useState("");
 
     useEffect(() => {
         getMovieData(movieId);
     }, []);
 
-    const getSingleMovieData = () => {
-        axios
-            .get(`http://localhost:8080/api/v1/reviews/movies/${movieId}`)
-            .then((res) => {
-                setGetReview(res.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
+    const loadReviews = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8080/api/v1/reviews/movies/${movieId}`);
+            setGetReview(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     useEffect(() => {
-        getSingleMovieData();
-        const interval = setInterval(getSingleMovieData, 1000); // Fetch data every 5 seconds
-        return () => clearInterval(interval); // Clear interval on component unmount
+        loadReviews();
+        const interval = setInterval(loadReviews, 1000);
+        return () => clearInterval(interval);
     }, [movieId]);
 
-    const addReview = (e) => {
-        e.preventDefault();
 
-        const rev = revText.current;
+    const addReview = async (event) => {
+        event.preventDefault();
 
-        const reviewData = {
-            reviewBody: rev.value,
-            imdbId: movieId,
-        };
-
-        axios
-            .post("http://localhost:8080/api/v1/reviews", reviewData)
-            .then((response) => {
-                const updatedReviews = [...(reviews || []), { body: rev.value }];
-                rev.value = "";
-                setReviews(updatedReviews);
-                setReview(updatedReviews);
-            })
-            .catch((error) => {
-                console.error(error);
+        try {
+            await axios.post("http://localhost:8080/api/v1/reviews", {
+                reviewBody: rev,
+                imdbId: movieId,
             });
+            setRev("");
+            loadReviews();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const editReview = (review) => {
+        setEditingReview(review);
+        setRev(review.body);
+    };
+
+    const updateReview = async (event) => {
+        event.preventDefault();
+
+        try {
+            await axios.put(`http://localhost:8080/api/v1/reviews/${editingReview.id}`, {
+                reviewBody: rev,
+            });
+            setEditingReview(null);
+            setRev("");
+            loadReviews();
+        } catch (error) {
+            console.error(error);
+        }
     };
 
 
-    const deleteReview = (id) => {
-        axios
-            .delete("http://localhost:8080/api/v1/reviews/" + id)
-            .then(() => {
-                alert("Document Deleted Successfully!");
-            })
-            .catch(() => {
-                alert("Error Occurred On Delete");
-            });
-    }
+    const deleteReview = async (id) => {
+        try {
+            await axios.delete(`http://localhost:8080/api/v1/reviews/${id}`);
+            loadReviews();
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
 
     return (
         <Container>
@@ -80,26 +90,56 @@ const Reviews = ({ getMovieData, movie, reviews = [], setReviews }) => {
                     <img src={movie?.poster} alt="" />
                 </Col>
                 <Col>
-                    <>
-                        <Row>
-                            <Col>
-                                <ReviewForm handleSubmit={addReview} revText={revText} lableText="Write a Review?" />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <hr />
-                            </Col>
-                        </Row>
-                    </>
-                    {getreview?.map((r, index) => (
+                    <Container className="mb-3">
+                        <h1 className="text-left text-white mb-3">Reviews</h1>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="reviewBody">
+                                <FormControl
+                                    type="text"
+                                    as="textarea"
+                                    value={rev}
+                                    rows={3}
+                                    onChange={(event) => setRev(event.target.value)}
+                                />
+                            </Form.Group>
+
+                            <div className="text-center">
+                                {editingReview ? (
+                                    <Button className="me-2" variant='outline-warning' onClick={updateReview}>
+                                        Update
+                                    </Button>
+                                ) : (
+                                    <Button className="me-2" variant='outline-info' onClick={addReview}>
+                                        Submit
+                                    </Button>
+                                )}
+                                <Button variant="danger" onClick={() => setEditingReview(null)}>
+                                    Cancel
+                                </Button>
+                            </div>
+                        </Form>
+                    </Container>
+                    <br />
+                    {getreview?.map((review, index) => (
                         <div key={index}>
                             <Row>
-                                <Col>{r.body}</Col>
-                                <Button>Edit</Button>
-                                <Button onClick={() =>
-                                    deleteReview(r.id)
-                                }>Delete</Button>
+                                <Col>{review.body}</Col>
+                                <Button
+                                    className="me-2"
+                                    variant="warning"
+                                    onClick={() => editReview(review)}
+                                    style={{ backgroundColor: "#ffc107" }}
+                                >
+                                    Edit
+                                </Button>
+
+                                <Button
+                                    variant="danger"
+                                    onClick={() => deleteReview(review.id)}
+                                    style={{ backgroundColor: "#dc3545" }}
+                                >
+                                    Delete
+                                </Button>
                             </Row>
                             <Row>
                                 <Col>
@@ -118,6 +158,8 @@ const Reviews = ({ getMovieData, movie, reviews = [], setReviews }) => {
         </Container>
     );
 };
+
+
 
 
 export default Reviews;
